a442d7fc1946a1495c5eceedbb57bdb9553665bf
Kenneth Rose <rosekenny12@gmail.com>
2023-12-23 23:38:51 -0500

    Clients can sent text messages to each other

--- client/client.js
+++ client/client.js
@@ -1,28 +1,56 @@
-document.addEventListener('DOMContentLoaded', function () {
-	const ownUsernameElement = document.getElementById('ownUsername')
-	const usernamesListElement = document.getElementById('usernamesList')
-	let ownUsername = ''
-	const ws = new WebSocket('ws://192.168.2.1:8080')
+document.addEventListener("DOMContentLoaded", function () {
+	const ownUsernameElement = document.getElementById("ownUsername");
+	const usernamesListElement = document.getElementById("usernamesList");
+	let ownUsername = "";
+	const ws = new WebSocket("ws://192.168.2.1:8080");
 
+	// Event listener for incoming WebSocket messages
 	ws.onmessage = function (event) {
-		const data = JSON.parse(event.data)
-		if (data.type === 'username') {
-			ownUsername = data.username
-			ownUsernameElement.textContent = 'You are: ' + ownUsername
-		} else if (data.type === 'usernames') {
-			const usernames = data.usernames
-			usernamesListElement.innerHTML = '' // Clear the list
+		const data = JSON.parse(event.data);
+
+		if (data.type === "username") {
+			// Handle assignment of username
+
+			ownUsername = data.username;
+			ownUsernameElement.textContent = "You are: " + ownUsername;
+		} else if (data.type === "usernames") {
+			// Handle list of other users
+
+			const usernames = data.usernames;
+			usernamesListElement.innerHTML = ""; // Clear the list
+
+			// Iterate over list of usernames
 			for (let i = 0; i < usernames.length; i++) {
 				if (usernames[i] !== ownUsername) {
-					const li = document.createElement('li')
-					li.textContent = usernames[i]
-					usernamesListElement.appendChild(li)
+					// Add username to the webpage
+					const li = document.createElement("li");
+					li.textContent = usernames[i];
+
+					// When a username from the list is clicked...
+					li.addEventListener("click", function () {
+						// Prompt the user for a message...
+						const message = prompt("Enter your message:");
+						if (message !== null) {
+							// And send the message to the clicked username
+							ws.send(
+								JSON.stringify({
+									type: "message",
+									to: usernames[i],
+									message: message,
+								})
+							);
+						}
+					});
+					usernamesListElement.appendChild(li);
 				}
 			}
+		} else if (data.type === "message") {
+			// Recieve message from other user
+			alert(`Message from ${data.username}: ${data.message}`);
 		}
-	}
+	};
 
 	window.onbeforeunload = function () {
-		ws.close()
-	}
-})
+		ws.close();
+	};
+});
--- server/server.js
+++ server/server.js
@@ -1,74 +1,98 @@
-const WebSocket = require('websocket').server
-const http = require('http')
+const WebSocket = require("websocket").server;
+const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log('Received request for', req.url)
-	res.writeHead(404)
-	res.end()
-})
+	console.log("Received request for", req.url);
+	res.writeHead(404);
+	res.end();
+});
 
 server.listen(8080, () => {
-	console.log('Server is listening on port 8080')
-})
+	console.log("Server is listening on port 8080");
+});
 
 const wsServer = new WebSocket({
 	httpServer: server,
-})
+});
 
 const generateUsername = () => {
-	const adjectives = ['Happy', 'Lucky', 'Clever', 'Cool', 'Energetic']
-	const nouns = ['Cat', 'Dog', 'Fox', 'Tiger', 'Rabbit']
+	const adjectives = ["Happy", "Lucky", "Clever", "Cool", "Energetic"];
+	const nouns = ["Cat", "Dog", "Fox", "Tiger", "Rabbit"];
 
-	let username = ''
+	let username = "";
 
 	do {
-		const adjective =
-			adjectives[Math.floor(Math.random() * adjectives.length)]
-		const noun = nouns[Math.floor(Math.random() * nouns.length)]
-		username = `${adjective}${noun}`
-	} while (clients.some((client) => client.username === username))
+		const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+		const noun = nouns[Math.floor(Math.random() * nouns.length)];
+		username = `${adjective}${noun}`;
+	} while (clients.some((client) => client.username === username));
 
-	return username
-}
+	return username;
+};
 
-const clients = []
+const clients = [];
 
 const broadcastUsernames = () => {
-	const usernames = clients.map((client) => client.username)
+	const usernames = clients.map((client) => client.username);
+	const messages = clients.reduce((allMessages, client) => {
+		return allMessages.concat(
+			client.messages.map((message) => ({
+				username: client.username,
+				message,
+				recipient: client.recipient,
+			}))
+		);
+	}, []);
 	clients.forEach((client) => {
 		client.connection.sendUTF(
-			JSON.stringify({ type: 'usernames', usernames })
-		)
-	})
-}
+			JSON.stringify({ type: "usernames", usernames, messages })
+		);
+	});
+};
 
-wsServer.on('request', (request) => {
-	const connection = request.accept(null, request.origin)
-	let username = generateUsername()
+wsServer.on("request", (request) => {
+	const connection = request.accept(null, request.origin);
+	let username = generateUsername();
 
-	const client = { connection, username }
-	clients.push(client)
+	const client = { connection, username, messages: [], recipient: null };
+	clients.push(client);
 
-	connection.sendUTF(JSON.stringify({ type: 'username', username }))
-	broadcastUsernames() // Send updated usernames to all clients
+	connection.sendUTF(JSON.stringify({ type: "username", username }));
+	broadcastUsernames();
 
-	connection.on('message', (message) => {
-		if (message.type === 'utf8') {
-			username = generateUsername()
-			client.username = username
-
-			connection.sendUTF(JSON.stringify({ type: 'username', username }))
-			broadcastUsernames() // Send updated usernames to all clients
-
-			console.log(`${username} connected`)
+	connection.on("message", (message) => {
+		if (message.type === "utf8") {
+			const data = JSON.parse(message.utf8Data);
+			if (
+				data.type === "message" &&
+				typeof data.message === "string" &&
+				typeof data.to === "string"
+			) {
+				const recipientClient = clients.find((c) => c.username === data.to);
+				if (recipientClient) {
+					recipientClient.messages.push(data.message);
+					console.log(data.message);
+					recipientClient.recipient = client.username;
+					recipientClient.connection.sendUTF(
+						JSON.stringify({
+							type: "message",
+							username: client.username,
+							message: data.message,
+						})
+					);
+				}
+				client.recipient = data.to;
+				broadcastUsernames();
+			}
 		}
-	})
+	});
 
-	connection.on('close', () => {
-		const index = clients.indexOf(client)
+	connection.on("close", () => {
+		// Remove the client from the list of connected clients
+		const index = clients.indexOf(client);
 		if (index !== -1) {
-			clients.splice(index, 1)
-			broadcastUsernames() // Send updated usernames to all clients
+			clients.splice(index, 1);
+			broadcastUsernames();
 		}
-	})
-})
+	});
+});
